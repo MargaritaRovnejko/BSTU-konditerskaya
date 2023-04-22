7eb1c22466b52c6fb6f772abbbb5d9f20078746f
MargaritaRovnejko <em000321@g.bstu.by>
2023-04-22 21:09:44 +0300

    refactor: add additional functions
    Добавлены дополнительные функции в калькулятор

--- calc.js
+++ calc.js
@@ -9,4 +9,11 @@ function calcCost(){
     document.getElementById('priceProduct').innerHTML = total;
     document.getElementById('infoProduct').style.display='block';
 
+    if (sP==0 || qP==0)
+    {
+        document.getElementById('infoProduct').style.display = 'none';
+        alert('Укажите значения');
+        return;
+    }
+    
 }
