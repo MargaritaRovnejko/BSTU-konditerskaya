4565c46f521ff9abaa7810f26cb7fd81cc218ca3
MargaritaRovnejko <em000321@g.bstu.by>
2023-04-22 21:07:29 +0300

    refactor: delete the odd action
    Удалено лишнее действие в калькуляторе

--- calc.js
+++ calc.js
@@ -9,11 +9,4 @@ function calcCost(){
     document.getElementById('priceProduct').innerHTML = total;
     document.getElementById('infoProduct').style.display='block';
 
-    if (sP==0 || qP==0)
-    {
-        document.getElementById('infoProduct').style.display = 'none';
-        alert('Укажите значения');
-        return;
-    }
-
 }
